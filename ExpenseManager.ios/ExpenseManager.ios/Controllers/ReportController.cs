fc2721d3b04e671029e26baa6a73875de876f4c9
Daniel <daniel.refahi@hotmail.com>
2017-07-12 23:15:55 +1000

    adding sample chart to report page

--- ExpenseManager.ios/ExpenseManager.ios/Controllers/ReportController.cs
+++ ExpenseManager.ios/ExpenseManager.ios/Controllers/ReportController.cs
@@ -1,18 +1,48 @@
 using Foundation;
 using System;
 using UIKit;
+using Syncfusion.SfChart.iOS;
+using ExpenseManager.ios.ListSources;
 
 namespace ExpenseManager.ios
 {
-    public partial class ReportController : UICollectionViewController
+    public partial class ReportController : UIViewController
     {
-        public ReportController (IntPtr handle) : base (handle)
+        public ReportController(IntPtr handle) : base(handle)
         {
         }
 
         public override void ViewDidAppear(bool animated)
         {
             base.ViewDidAppear(animated);
-        }
+
+			SFChart chart = new SFChart();
+			chart.Frame = this.View.Frame;
+
+			//Define the title for the Chart.
+			chart.Title.Text = new NSString("Weather Analysis");
+
+			//Adding legend to the Chart.
+			chart.Legend.Visible = true;
+
+			//Adding Primary Axis for the Chart.
+			SFCategoryAxis primaryAxis = new SFCategoryAxis();
+			primaryAxis.Title.Text = new NSString("Month");
+			chart.PrimaryAxis = primaryAxis;
+
+			//Adding Secondary Axis for the Chart.
+			SFNumericalAxis secondaryAxis = new SFNumericalAxis();
+			secondaryAxis.Title.Text = new NSString("Temperature");
+			chart.SecondaryAxis = secondaryAxis;
+
+			//Defining the data source for the Chart.
+			var dataModel = new ReportChartDataSource();
+			chart.DataSource = dataModel as SFChartDataSource;
+
+			//Adding our Chart as a sub view.
+			this.View.AddSubview(chart);
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
     }
 }
---
+++ ExpenseManager.ios/ExpenseManager.ios/ListSources/ReportChartDataSource.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Foundation;
+using Syncfusion.SfChart.iOS;
+
+namespace ExpenseManager.ios.ListSources
+{
+    public class ReportChartDataSource : SFChartDataSource
+    {
+		NSMutableArray highTemperature;
+		NSMutableArray lowTemperature;
+		NSMutableArray precipitation;
+
+		public ReportChartDataSource()
+		{
+			highTemperature = new NSMutableArray();
+			lowTemperature = new NSMutableArray();
+			precipitation = new NSMutableArray();
+
+			AddDataPointsForChart("Jan", 42, 27, 3.03);
+			AddDataPointsForChart("Feb", 44, 28, 2.48);
+			AddDataPointsForChart("Mar", 53, 35, 3.23);
+			AddDataPointsForChart("Apr", 64, 44, 3.15);
+			AddDataPointsForChart("May", 75, 54, 4.13);
+			AddDataPointsForChart("Jun", 83, 63, 3.23);
+			AddDataPointsForChart("Jul", 87, 68, 4.13);
+			AddDataPointsForChart("Aug", 84, 66, 4.88);
+			AddDataPointsForChart("Sep", 78, 59, 3.82);
+			AddDataPointsForChart("Oct", 67, 48, 3.07);
+			AddDataPointsForChart("Nov", 55, 38, 2.83);
+			AddDataPointsForChart("Dec", 45, 29, 2.80);
+		}
+
+		/// <summary>
+		/// Method to populate the array required for all the series.
+		/// </summary>
+		/// <param name="month">Month.</param>
+		/// <param name="high">High Temperature.</param>
+		/// <param name="low">Low Temperature.</param>
+		/// <param name="ppt">Precipitation.</param>
+		void AddDataPointsForChart(String month, Double high, Double low, Double ppt)
+		{
+			highTemperature.Add(new SFChartDataPoint(NSObject.FromObject(month), NSObject.FromObject(high)));
+
+			lowTemperature.Add(new SFChartDataPoint(NSObject.FromObject(month), NSObject.FromObject(low)));
+
+			precipitation.Add(new SFChartDataPoint(NSObject.FromObject(month), NSObject.FromObject(ppt)));
+		}
+
+		[Export("numberOfSeriesInChart:")]
+		public override nint NumberOfSeriesInChart(SFChart chart)
+		{
+			return 3;
+			//returns no of series required for the chart.
+		}
+
+		[Export("chart:seriesAtIndex:")]
+		public override SFSeries GetSeries(SFChart chart, nint index)
+		{
+			//returns the series for the chart.
+			if (index == 1)
+			{
+				SFSplineSeries series = new SFSplineSeries();
+				series.Label = new NSString("High");
+				return series;
+			}
+			else if (index == 2)
+			{
+				SFSplineSeries series = new SFSplineSeries();
+				series.Label = new NSString("Low");
+				return series;
+			}
+			else
+			{
+				SFColumnSeries series = new SFColumnSeries();
+				series.Label = new NSString("Precipitation");
+
+				SFNumericalAxis axis = new SFNumericalAxis();
+				axis.OpposedPosition = true;
+				axis.ShowMajorGridLines = false;
+				series.YAxis = axis;
+
+				return series;
+			}
+		}
+
+		[Export("chart:dataPointAtIndex:forSeriesAtIndex:")]
+		public override SFChartDataPoint GetDataPoint(SFChart chart, nint index, nint seriesIndex)
+		{
+			//returns the datapoint for each series.
+			if (seriesIndex == 1)
+			{
+				return highTemperature.GetItem<SFChartDataPoint>((nuint)index);
+			}
+			else if (seriesIndex == 2)
+			{
+				return lowTemperature.GetItem<SFChartDataPoint>((nuint)index);
+			}
+			else
+			{
+				return precipitation.GetItem<SFChartDataPoint>((nuint)index);
+			}
+		}
+
+		[Export("chart:numberOfDataPointsForSeriesAtIndex:")]
+		public override nint GetNumberOfDataPoints(SFChart chart, nint index)
+		{
+			return 12;
+			//No of datapoints needed for each series.
+		}
+    }
+}
